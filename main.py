736e7df7874489ea7e7bceb964ec18b0c6d45a14
chris <cndruta@email.arizona.edu>
2018-11-29 21:01:54 -0700

    Messing around with peak finding

--- main.py
+++ main.py
@@ -20,15 +20,29 @@ numFreqBins, numSamples = spectogramX.shape
 print(numFreqBins, numSamples)
 
 freqsAt0 = spectogramX[:,0]
+noise = np.random.normal(60,5,len(freqsAt0))
+
+freqsAt0Noise = freqsAt0 + noise
+#wc = 2 * np.pi * 21000 / fs
+#r = 10
+#b = [1, -r*(np.e**(1j*wc) + np.e**(-1j*wc)), r**2]
+#a = [-1, 0, -1]
+
+#noise = np.abs(signal.lfilter(b, a, noise))
+
+ind = signal.find_peaks(freqsAt0)[0]
+#ind = signal.find_peaks_cwt(freqsAt0, np.arange(1,10))
 
 if 1:
     plt.figure()
     plt.title('DTFT of mono channel Space Jam at t=0')
     plt.plot(f, freqsAt0)
+    plt.stem(f[ind],freqsAt0[ind], linefmt=':', basefmt=' ')
+    #plt.stem(f, noise)
     plt.xlabel('Frequency [Hz]')
     plt.ylabel('dB')
 
-if 1:
+if 0:
     plt.figure()
     plt.pcolormesh(t, f, spectogramX)
     plt.ylabel('Frequency [Hz]')
