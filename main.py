a5519c1417710eeb6d72b02b2c80714b1cb3a4e3
chris <cndruta@email.arizona.edu>
2018-12-04 15:00:26 -0700

    Got time axis finger print filter down

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ import matplotlib
 matplotlib.use('Qt5Agg')
 import matplotlib.pyplot as plt
 
+from scipy import stats
 from scipy import signal
 from scipy.io import wavfile
 
 import sounddevice as sd
-sd.default.device = 7
+#sd.default.device = 7
 
 clipDuration = 5
 
@@ -37,31 +38,48 @@ plt.title('test')"""
 
 dirtyData = cleanData * 0.1 + noise * 1.5
 dirtyData = dirtyData.astype(np.int16)
-#sd.play(dirtyData, fs)
+sd.play(dirtyData, fs)
 
 fxClean, txClean, spectrogramClean = signal.spectrogram(cleanData, fs)
 spectrogramClean[spectrogramClean == -np.inf] = 0
 spectrogramClean = 20 * np.log10(spectrogramClean)
+spectrogramClean = spectrogramClean/np.amax(spectrogramClean)
 
 fxDirty, txDirty, spectrogramDirty = signal.spectrogram(dirtyData, fs)
 spectrogramDirty[spectrogramDirty == -np.inf] = 0
 spectrogramDirty = 20 * np.log10(spectrogramDirty)
-
-freqsAt0Clean = spectrogramClean[:,0]
-freqsAt0Dirty = spectrogramDirty[:,0]
+spectrogramDirty = spectrogramDirty/np.amax(spectrogramDirty)
 
 times = []
 peaksClean = []
 peaksDirty = []
 window = [10]
+test = []
 
-for i in range(0, spectrogramClean.shape[1], 15):
+highpassWc = 5000
+# Shitty highpass
+for i, f in enumerate(fxClean):
+    if f > highpassWc:
+        highpassIndex = i
+        break
+
+for i in range(0, spectrogramClean.shape[1]):
+
+    test.append(np.sum(spectrogramClean[highpassIndex:,i]))
+
     times.append(i)
     freqsAtiClean = spectrogramClean[:,i]
     freqsAtiDirty = spectrogramDirty[:,i]
     
     peaksClean.append(signal.find_peaks_cwt(freqsAtiClean, window))
     peaksDirty.append(signal.find_peaks_cwt(freqsAtiDirty, window))
+
+hmm = [round(t) for t in test]
+hmmAmp = [h * 200 for h in hmm]
+hmmX = txClean[np.arange(0, len(hmm), 1)]
+
+window2 = [1]
+peaks = signal.find_peaks_cwt(hmm, window2)
 
 if 1:
     plt.figure()
@@ -71,9 +89,11 @@ if 1:
     plt.ylabel('Frequency [Hz]')
     plt.xlabel('Time [sec]')
     plt.colorbar()
+    plt.plot(hmmX, hmmAmp, '-k')
+    plt.stem(txClean[peaks], [fxClean[60]] * len(txClean[peaks]))
 
-    for xe, ye in zip(times, peaksClean):
-        plt.scatter([txClean[xe]] * len(ye), fxClean[ye], edgecolors='black')
+    #for xe, ye in zip(times, peaksClean):
+    #    plt.scatter([txClean[xe]] * len(ye), fxClean[ye], edgecolors='black')
 
     plt.title('Clean spectrogram')
 
@@ -83,10 +103,9 @@ if 1:
     plt.xlabel('Time [sec]')
     plt.colorbar()
 
-    for xe, ye in zip(times, peaksDirty):
-        plt.scatter([txDirty[xe]] * len(ye), fxDirty[ye], edgecolors='black')
+    #for xe, ye in zip(times, peaksDirty):
+    #    plt.scatter([txDirty[xe]] * len(ye), fxDirty[ye], edgecolors='black')
     
     plt.title('Dirty spectrogram')
-
-plt.tight_layout()
-plt.show()
+    plt.tight_layout()
+    plt.show()
